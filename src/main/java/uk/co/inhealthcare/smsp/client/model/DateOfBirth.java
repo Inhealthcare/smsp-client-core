201dd52a667902c60b25bb42f30a4de6f70bd84e
r_croft <richard.croft@inhealthcare.co.uk>
2017-03-13 14:49:43 +0000

    Removed restriction on date of birth format to allow partial dates

--- src/main/java/uk/co/inhealthcare/smsp/client/model/DateOfBirth.java
+++ src/main/java/uk/co/inhealthcare/smsp/client/model/DateOfBirth.java
@@ -1,21 +1,14 @@
 package uk.co.inhealthcare.smsp.client.model;
 
-import java.text.ParseException;
-
 import org.apache.commons.lang3.StringUtils;
-import org.apache.commons.lang3.time.DateUtils;
 
 public class DateOfBirth {
 
 	private String date;
 
 	public DateOfBirth(String date) {
-		if (StringUtils.isBlank(date))
+		if (StringUtils.isBlank(date)) {
 			throw new IllegalArgumentException("Cannot have blank data of birth");
-		try {
-			DateUtils.parseDate(date, "yyyyMMdd");
-		} catch (ParseException e) {
-			throw new IllegalArgumentException("Date of birth is not of correct format yyyyMMdd");
 		}
 		this.date = date;
 	}
